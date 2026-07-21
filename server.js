c65d19dbc95adc1a98d7e892749e99a36164d48e
Boma-App678 <ahmedibrahimmatar678@gmail.com>
2026-07-21 20:59:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -18,8 +18,9 @@ app.set('trust proxy', 1);
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
+// 🚀 التعديل الجديد: تم تغيير نطاق السماح ليطابق رابط GitHub الجديد 🚀
 app.use(cors({
-    origin: '*',
+    origin: ['https://boma-app678.github.io', 'http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'authorization', 'x-admin-pass']
 }));
